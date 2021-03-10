82d82232a2968c3540283f4cb943d68d2791765f
HigorLapa-MBP <higor.lapa@cloudwalk.io>
2021-03-10 08:51:09 -0300

    feat: migrates the app to null-safety; bumps version to 2.0.0

--- lib/advertising_id.dart
+++ lib/advertising_id.dart
@@ -3,15 +3,15 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class AdvertisingId {
-  static const MethodChannel _channel =
-      const MethodChannel('advertising_id');
+  static const MethodChannel _channel = const MethodChannel('advertising_id');
 
   static Future<String> get id async {
-    final String id = await _channel.invokeMethod('getAdvertisingId');
-    return id;
+    final String? id = await _channel.invokeMethod('getAdvertisingId');
+    return id ?? "";
   }
 
   static Future<bool> get isLimitAdTrackingEnabled async {
-    return await _channel.invokeMethod('isLimitAdTrackingEnabled');
+    final result = await _channel.invokeMethod('isLimitAdTrackingEnabled');
+    return result ?? false;
   }
 }
